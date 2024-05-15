905a1a7ca4a640d2542dc0521bf67e7e894151c2
Felipe Nascimento <flpnascto@gmail.com>
2024-05-15 20:29:01 -0300

    feat: comand server with otel

--- goapp/cmd/server/main.go
+++ goapp/cmd/server/main.go
@@ -1,58 +1,117 @@
 package main
 
 import (
-	"encoding/json"
+	"context"
+	"fmt"
+	"log"
 	"net/http"
+	"os"
+	"os/signal"
 	"strings"
+	"time"
 
-	"github.com/flpnascto/otel-go/goapp/configs"
-	"github.com/flpnascto/otel-go/goapp/internal/entity"
-	"github.com/flpnascto/otel-go/goapp/internal/infra/api"
+	"github.com/flpnascto/otel-go/goapp/internal/infra/web"
+	"github.com/joho/godotenv"
+	"github.com/spf13/viper"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+
+	"go.opentelemetry.io/otel"
+	"go.opentelemetry.io/otel/exporters/otlp/otlptrace/otlptracegrpc"
+	"go.opentelemetry.io/otel/propagation"
+	"go.opentelemetry.io/otel/sdk/resource"
+	sdktrace "go.opentelemetry.io/otel/sdk/trace"
+	semconv "go.opentelemetry.io/otel/semconv/v1.21.0"
 )
 
-func main() {
-	configs, err := configs.LoadConfig(".")
+func initProvider(serviceName, collectorURL string) (func(context.Context) error, error) {
+	ctx := context.Background()
+
+	res, err := resource.New(ctx,
+		resource.WithAttributes(
+			semconv.ServiceName(serviceName),
+		),
+	)
 	if err != nil {
-		panic(err)
+		return nil, fmt.Errorf("failed to create resource: %w", err)
+	}
+	ctx, cancel := context.WithTimeout(ctx, time.Second)
+	defer cancel()
+	conn, err := grpc.DialContext(ctx, collectorURL,
+		grpc.WithTransportCredentials(insecure.NewCredentials()),
+		grpc.WithBlock(),
+	)
+	if err != nil {
+		return nil, fmt.Errorf("failed to create gRPC connection to collector: %w", err)
 	}
 
-	mux := http.NewServeMux()
-	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		parts := strings.Split(r.URL.Path, "/")
+	traceExporter, err := otlptracegrpc.New(ctx, otlptracegrpc.WithGRPCConn(conn))
+	if err != nil {
+		return nil, fmt.Errorf("failed to create trace exporter: %w", err)
+	}
 
-		cepQuery := parts[len(parts)-1]
-		cep, err := entity.NewCep(cepQuery)
-		if err != nil {
-			http.Error(w, "invalid zipcode", http.StatusUnprocessableEntity)
-			return
+	bsp := sdktrace.NewBatchSpanProcessor(traceExporter)
+	tracerProvider := sdktrace.NewTracerProvider(
+		sdktrace.WithSampler(sdktrace.AlwaysSample()),
+		sdktrace.WithResource(res),
+		sdktrace.WithSpanProcessor(bsp),
+	)
+	otel.SetTracerProvider(tracerProvider)
+
+	otel.SetTextMapPropagator(propagation.TraceContext{})
+
+	return tracerProvider.Shutdown, nil
+}
+
+func init() {
+	if err := godotenv.Load(); err != nil {
+		log.Println("No .env file found")
+	}
+	viper.AutomaticEnv()
+	viper.SetEnvKeyReplacer(strings.NewReplacer(".", "_"))
+}
+
+func main() {
+	sigCh := make(chan os.Signal, 1)
+	signal.Notify(sigCh, os.Interrupt)
+
+	ctx, cancel := signal.NotifyContext(context.Background(), os.Interrupt)
+	defer cancel()
+
+	shutdown, err := initProvider(viper.GetString("OTEL_SERVICE_NAME"), viper.GetString("OTEL_EXPORTER_OTLP_ENDPOINT"))
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer func() {
+		if err := shutdown(ctx); err != nil {
+			log.Fatal("failed to shutdown TracerProvider: %w", err)
 		}
+	}()
 
-		city, err := api.FetchCepApi(cep)
-		if err != nil {
-			http.Error(w, "can not find zipcode", http.StatusNotFound)
-			return
+	tracer := otel.Tracer("microservice-tracer")
+
+	templateData := &web.TemplateData{
+		RequestNameOTEL: viper.GetString("REQUEST_NAME_OTEL"),
+		OTELTracer:      tracer,
+	}
+	server := web.NewServer(templateData)
+	router := server.CreateServer()
+
+	go func() {
+		log.Println("Starting server on port", viper.GetString("HTTP_PORT"))
+		if err := http.ListenAndServe(viper.GetString("HTTP_PORT"), router); err != nil {
+			log.Fatal(err)
 		}
+	}()
 
-		temp, err := api.FetchWeatherApi(*city, configs.WeatherApiKey)
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
-		}
+	select {
+	case <-sigCh:
+		log.Println("Shutting down gracefully, CTRL+C pressed...")
+	case <-ctx.Done():
+		log.Println("Shutting down due to other reason...")
+	}
 
-		response := map[string]any{
-			"city":   city,
-			"temp_C": temp.TempC,
-			"temp_F": temp.TempF,
-			"temp_K": temp.TempK,
-		}
-
-		responseBytes, err := json.Marshal(response)
-		if err != nil {
-			panic(err)
-		}
-
-		w.Write(responseBytes)
-	})
-
-	http.ListenAndServe(":8080", mux)
+	_, shutdownCancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer shutdownCancel()
 }
